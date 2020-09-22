1c82094d98bd2ec14d8a10057ca06c1187584ab5
longgangsima <longgangsima@gmail.com>
2020-09-22 14:34:32 -0400

    Updates

---
+++ precache-manifest.17ebebbec9a0991438a9a30d69832f0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d58b34e596a0c2aeac91f124593981",
+    "url": "/popular-move-app/index.html"
+  },
+  {
+    "revision": "f07c1dc1cd8e44a01829",
+    "url": "/popular-move-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b751adf253ff05245ab6",
+    "url": "/popular-move-app/static/js/2.0f7eca22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/popular-move-app/static/js/2.0f7eca22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07c1dc1cd8e44a01829",
+    "url": "/popular-move-app/static/js/main.cf7a4c6c.chunk.js"
+  },
+  {
+    "revision": "da2fa1e65c3036acae26",
+    "url": "/popular-move-app/static/js/runtime-main.fe12fdd7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/popular-move-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
