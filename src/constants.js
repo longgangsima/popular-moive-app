2f3d6687b22da1a9e36c55c5c1e8b3ca76d106a0
longgangsima <longgangsima@gmail.com>
2020-09-24 19:38:32 -0400

    combine oliva and suguo

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+// for redux action types
+export const LOAD_DATA_FROM_DB = "load_data_from_db";
+export const ADD_TO_FAVORITE = "add_to_favorite";
+export const REMOVE_FROM_FAVORITE = "remove_from_favorite";
+export const ADD_TO_BLOCK = "add_to_block";
+export const REMOVE_FROM_BLOCK = "remove_from_block";
