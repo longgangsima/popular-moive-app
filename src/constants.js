3652b3f2a6a7f1a45c6693313d9e2fcbbbd91af2
Su Guo <42634219+SaoriKaku@users.noreply.github.com>
2020-09-25 16:58:31 -0700

    update redux + component movie tab

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+// for redux action types
+export const LOAD_NEXT_PAGE_FROM_DB = "load_next_page_from_db";
+export const ADD_TO_FAVORITE = "add_to_favorite";
+export const REMOVE_FROM_FAVORITE = "remove_from_favorite";
+export const ADD_TO_BLOCK = "add_to_block";
+export const REMOVE_FROM_BLOCK = "remove_from_block";
+// for url
+export const URL_IMAGE_PREFIX = "https://image.tmdb.org/t/p/w500/";
+export const URL_DISCOVER_PREFIX = "https://api.themoviedb.org/3";
+export const API_KEY_SUGUO = "98d76f4e69ff80643c26ba75b21f9352";
