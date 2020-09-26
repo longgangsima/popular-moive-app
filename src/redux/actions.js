c091a67a5b067bc6b2057171bc1156e7799835b7
Su Guo <42634219+SaoriKaku@users.noreply.github.com>
2020-09-25 17:01:21 -0700

    update redux + component movie tab

---
+++ src/redux/actions.js
@@ -0,0 +1,52 @@
+// for action creator
+import {LOAD_NEXT_PAGE_FROM_DB, ADD_TO_FAVORITE, REMOVE_FROM_FAVORITE, ADD_TO_BLOCK, REMOVE_FROM_BLOCK,
+    URL_DISCOVER_PREFIX, API_KEY_SUGUO} from "../constants";
+import axios from "axios";
+
+const loadNextPageFromDBSynch = (singlePageMovieList) => ({
+    type: LOAD_NEXT_PAGE_FROM_DB,
+    data: singlePageMovieList,
+});
+
+export const loadNextPageFromDB = (page) => {
+    return (dispatch) => {
+        // send ajax to movieDB
+        const url = URL_DISCOVER_PREFIX + "/discover/movie?api_key=" + API_KEY_SUGUO 
+        + "&sort_by=popularity.desc&&include_adult=false&include_video=false&page=" + page;
+        axios.get(url)
+        .then(function (response) {
+            const nextPageMovieList = response.data.results;
+            // console.log("response", nextPageMovieList);
+            for(let movieItem of nextPageMovieList) {
+                movieItem.isFavorite = false;
+                movieItem.isBlock = false;
+            }
+            dispatch(loadNextPageFromDBSynch(nextPageMovieList));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+}
+
+export const addToFavorite = (movieItem) => ({
+    type: ADD_TO_FAVORITE,
+    data: movieItem,
+});
+
+export const removeFromFavorite = (movieId) => ({
+    type: REMOVE_FROM_FAVORITE,
+    data: movieId,
+});
+
+export const addToBlock = (movieItem) => ({
+    type: ADD_TO_BLOCK,
+    data: movieItem, 
+});
+
+export const removeFromBlock = (movieId) => ({
+    type: REMOVE_FROM_BLOCK,
+    data: movieId,
+});
+
+
