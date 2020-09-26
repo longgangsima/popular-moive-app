b85f3c7c4d38474568b98facb02e7f86c1dd7033
Su Guo <42634219+SaoriKaku@users.noreply.github.com>
2020-09-25 17:09:53 -0700

    update redux + component movie tab

---
+++ src/view/block-tab/block-tab.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const blockMovie = props => (
+    <div>BlockMovie component!</div>
+);
+
+export default blockMovie;
---
+++ src/view/home-tab/home-tab.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const home = props => (
+    <div>Home component!</div>
+);
+
+export default home;
