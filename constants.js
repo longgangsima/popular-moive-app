e436be48f1505b9cebb90b7650e4f00ca6ed6c66
Su Guo <42634219+SaoriKaku@users.noreply.github.com>
2020-09-27 15:19:45 -0700

    update constants + fix bugs page

---
+++ constants.js
@@ -0,0 +1,13 @@
+// for redux action types
+export const LOAD_NEXT_PAGE_FROM_DB = "load_next_page_from_db";
+export const ADD_TO_FAVORITE = "add_to_favorite";
+export const REMOVE_FROM_FAVORITE = "remove_from_favorite";
+export const ADD_TO_BLOCK = "add_to_block";
+export const REMOVE_FROM_BLOCK = "remove_from_block";
+// for url
+export const URL_IMAGE_PREFIX = "https://image.tmdb.org/t/p/w500/";
+export const URL_DISCOVER_PREFIX = "https://api.themoviedb.org/3";
+export const API_KEY_SUGUO = "98d76f4e69ff80643c26ba75b21f9352";
+// for current page action type
+export const ADD_CURRENT_PAGE = "add_current_page";
+export const DEDUCT_CURRENT_PAGE = "deduct_current_page";
