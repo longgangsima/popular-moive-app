7cb3ce8c690738c9f745ea2d927e2b00fdaaaf12
Su Guo <42634219+SaoriKaku@users.noreply.github.com>
2020-09-27 15:23:00 -0700

    update constants + fix bug page

--- src/constants.js
+++ src/constants.js
@@ -8,3 +8,6 @@ export const REMOVE_FROM_BLOCK = "remove_from_block";
 export const URL_IMAGE_PREFIX = "https://image.tmdb.org/t/p/w500/";
 export const URL_DISCOVER_PREFIX = "https://api.themoviedb.org/3";
 export const API_KEY_SUGUO = "98d76f4e69ff80643c26ba75b21f9352";
+// for current page action type
+export const ADD_CURRENT_PAGE = "add_current_page";
+export const DEDUCT_CURRENT_PAGE = "deduct_current_page";
